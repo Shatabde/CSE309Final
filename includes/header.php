40b655892cfb3e7f98596eeb530f15f69784bfba
MehenazRahman <114680757+Mehenaz-Rahman@users.noreply.github.com>
2023-12-12 23:49:56 +0600

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="Asset/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="Asset/font-awesome-4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="Asset/css/style.css">
+    <link rel="stylesheet" type="text/css" href="Asset/flickity/flickity.css">
+    <link rel="stylesheet" type="text/css" href="Asset/css/sweetalert.css">
+    <script type="text/javascript" src="Asset/flickity/flickity.js"></script>
+    <script type="text/javascript" src="Asset/sweetalert.min.js"></script>   
+    <title>Library Management</title>
+</head>
+<style> body {
+    background-image: url('Asset/img1.jpg');
+	background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: 100% 100%;
+	} 
+	</style>
+<body>
---
+++ includes/nav.php
@@ -0,0 +1,36 @@
+<?php 
+session_start();
+if (isset($_SESSION['admin'])) {
+     $admin = $_SESSION['admin'];
+}
+?>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example">
+                <span class="sr-only">:</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            
+            <a class="navbar-brand" href=""><img src="Asset/logo.png" alt="" width="30px" height="30px"></a>
+            <a class="navbar-brand" href="#">Book_Nest</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example">
+            <ul class="nav navbar-nav">
+                <?php if(isset($admin)) { ?>  
+                <li class="active"><a href="admin.php">Home</a></li>
+                <li><a href="bookstable.php">Books</a></li>
+                <li><a href="users.php">Admins</a></li>
+                <li><a href="viewstudents.php">Students</a></li>
+                <li><a href="borrowedbooks.php">Borrowed books</a></li>
+                <li><a href="fines.php">Fines</a></li>
+                <li><a href="logout.php">Logout</a></li>
+                <?php } ?>         
+            </ul> 
+        </div>
+    </div>
+</nav>
